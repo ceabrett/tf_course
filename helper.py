baae8567f87d652500c6a2e1c904c6122fc60393
Charles Brett <ceabrett@amazon.com>
2019-03-21 19:53:44 -0700

    helper

--- helper.py
+++ helper.py
@@ -1,2 +1,15 @@
-def hello():
-	print("hello")
+import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+def load_dataset_light():
+	df = pd.read_csv("tf_course/data.csv")
+	on_indices = df[df.state == 1].index
+	off_indices = df[df.state == 0].index
+	sampled_off_indices = np.random.choice(off_indices, len(on_indices), replace=False)
+	sampled_indices = np.concatenate([on_indices, sampled_off_indices])
+	np.random.shuffle(sampled_indices)
+	df_sampled = df.loc[sampled_indices]
+	label = df_sampled["state"]
+	feats_train, feats_test, label_train, label_test = train_test_split(feats, label, test_size=0.2, shuffle=False)
+	return feats_train, feats_test, label_train, label_test
